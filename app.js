efd4ede378aab439554d736bcdc0e2d1caea2660
Mario Zabala <mariogzabala@gmail.com>
2022-01-10 18:52:35 -0300

    creamos enlaces

--- app.js
+++ app.js
@@ -8,9 +8,31 @@ app.listen(3002, () =>{
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
-    
+    res.sendFile(path.join(__dirname, './views/index.html'))    
+});
+app.get('/ayuda', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ayuda.html'))    
+});
+app.get('/crea-tu-cuenta', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/crea-tu-cuenta.html'))    
+});
+app.get('/ingreso', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ingreso.html'))    
+});
+app.get('/mis-compras', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/mis-compras.html'))    
+});
+app.get('/ofertas', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ofertas.html'))    
+});
+app.get('/tiendas-oficiales', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/tiendas-oficiales.html'))    
+});
+app.get('/vender', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/vender.html'))    
 });
 
+
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/views')));
 
